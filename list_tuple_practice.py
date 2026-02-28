2fe904c2e2f9a97609c05ed06dc8ca3a3fc9e0bd
Kumar80-jpg <brockgold80@gmail.com>
2026-02-28 22:21:40 +0530

    Added list and tuple practice program

---
+++ list_tuple_practice.py
@@ -0,0 +1,31 @@
+# New Section
+
+fruit = ['Apple', 'Banana', 'Orange']
+
+print(fruit)
+
+fruit.append("Grapes")
+
+print(fruit)
+
+fruit.remove("Apple")
+
+print(fruit)
+
+fruit[2] = "fig"
+
+print(fruit)
+
+print(len(fruit))
+
+color = ('red', 'blue', 'green')
+
+print(color)
+
+color[2]
+
+color[2]= "white"
+
+type (color)
+
+type (fruit)
