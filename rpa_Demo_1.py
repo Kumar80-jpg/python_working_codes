20a21cd38e67ccdff08cc441208d4d4755f208f3
Kumar80-jpg <brockgold80@gmail.com>
2026-03-05 06:40:25 +0530

    Add files via upload

---
+++ rpa_Demo_1.py
@@ -0,0 +1,26 @@
+"""""
+import pyautogui
+import time 
+
+print("Hello World")
+
+#Mouse operations
+pyautogui.click(100,100)
+time.sleep(3)
+pyautogui.rightClick(100,100)
+
+time.sleep(10)
+
+pyautogui.rightClick(905,400)
+"""""
+
+#Keyboard Operations
+
+import time
+
+import pyautogui
+
+
+time.sleep(1)
+
+pyautogui.write("Kumar")
